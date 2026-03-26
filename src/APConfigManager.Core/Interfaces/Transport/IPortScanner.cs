8d5f6071cef42864bc0109522bb9b748aba76dd4
BMorozov <morozowb.p@gmail.com>
2026-03-26 19:19:25 +0100

    <feat>: Transports

---
+++ src/APConfigManager.Core/Interfaces/Transport/IPortScanner.cs
@@ -0,0 +1,24 @@
+namespace APConfigManager.Core.Interfaces.Transport
+{
+    /// <summary>
+    /// Discovery and monitoring of COM ports in the system(When the device reboots, the port will be change)
+    /// </summary>
+    public interface IPortScanner
+    {
+        /// <summary>
+        /// Returns list of available COM-ports.
+        /// </summary>
+        /// <returns></returns>
+        List<string> GetAvailablePorts();
+
+        /// <summary>
+        /// Waiting for a new port to appear (after reboot).
+        /// </summary>
+        Task<string?> WaitForNewPortAsync(List<string> existingPorts, TimeSpan timeout, CancellationToken ct);
+
+        /// <summary>
+        /// Waiting for the bootloader port (may differ from the original).
+        /// </summary>
+        Task<string?> WaitForBootloaderPortAsync(string originalPort, TimeSpan timeout, CancellationToken ct);
+    }
+}
---
+++ src/APConfigManager.Core/Interfaces/Transport/ISerialPortAdapter.cs
@@ -0,0 +1,42 @@
+namespace APConfigManager.Core.Interfaces.Transport
+{
+    /// <summary>
+    /// Abstraction over the COM port. Allows to test protocols without real hardware (substituting the moq),
+    /// </summary>
+    public interface ISerialPortAdapter : IDisposable
+    {
+        bool IsOpen { get; set; }
+
+        Stream BaseStream { get; set; }
+
+        /// <summary>
+        /// Open a port at the specified rate
+        /// </summary>
+        void Open(string port, int baudRate);
+
+        /// <summary>
+        /// Close a port.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Reading async data.
+        /// </summary>
+        Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken ct);
+
+        /// <summary>
+        /// Writing async data
+        /// </summary>
+        Task WriteAsync(byte[] data, int offset, int coun, CancellationToken ct);
+
+        /// <summary>
+        /// Change baud rate for COM-port
+        /// </summary>
+        void ChangeBaudRate(int baudRate);
+
+        /// <summary>
+        /// Flush buffer.
+        /// </summary>
+        void Flush();
+    }
+}
