0e33e460bd18cdc621e8e157910a6e476156c891
BMorozov <morozowb.p@gmail.com>
2026-07-01 20:14:31 +0200

    <FIX>: Format class

--- src/APConfigManager.Infrastructure/Drivers/Ardupilot/ArduPilotDriver.cs
+++ src/APConfigManager.Infrastructure/Drivers/Ardupilot/ArduPilotDriver.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Runtime.InteropServices;
 using APConfigManager.Core.Enums;
 using APConfigManager.Core.Exceptions;
 using APConfigManager.Core.Interfaces.Drivers;
 using APConfigManager.Core.Interfaces.Transport;
 using APConfigManager.Core.Models;
 using APConfigManager.Core.Results;
-using APConfigManager.Infrastructure.Transport;
-using static MAVLink;
 
 namespace APConfigManager.Infrastructure.Drivers.Ardupilot;
 
@@ -23,6 +19,19 @@ public class ArduPilotDriver : IAutopilotDriver
     private const int WriteParamsPasses = 3;
     private Action? onDeviceDisconnected;
     private readonly Func<Guid?, List<string>>? getOccupiedPorts;
+
+    private readonly ISerialPortAdapter port;
+    private readonly IBootloaderProtocol bootloader;
+    private readonly ITelemetryProtocol telemetry;
+    private readonly IPortScanner portScanner;
+
+    private DeviceSession? session;
+    private BootMode currentMode = BootMode.Normal;
+
+    // Telemetry
+    private CancellationTokenSource? telemetryCts;
+    private Task? telemetryTask;
+    private Action<float>? onAltitudeUpdate;
 
     private static readonly HashSet<string> ReadOnlyPrefixes = new(StringComparer.OrdinalIgnoreCase)
     {
@@ -47,11 +56,6 @@ public class ArduPilotDriver : IAutopilotDriver
         "BARO1_WCF_",
         "BARO2_WCF_",
     };
-
-    private readonly ISerialPortAdapter port;
-    private readonly IBootloaderProtocol bootloader;
-    private readonly ITelemetryProtocol telemetry;
-    private readonly IPortScanner portScanner;
 
     private static readonly byte[] CrcPad = { 0xFF, 0xFF, 0xFF, 0xFF };
 
@@ -90,14 +94,6 @@ public class ArduPilotDriver : IAutopilotDriver
         0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
         0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94, 0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
     };
-
-    private DeviceSession? session;
-    private BootMode currentMode = BootMode.Normal;
-
-    //Altitude telemetry
-    private CancellationTokenSource? telemetryCts;
-    private Task? telemetryTask;
-    private Action<float>? onAltitudeUpdate;
 
     /// <summary>
     /// Initializes the driver with protocol and transport dependencies.
